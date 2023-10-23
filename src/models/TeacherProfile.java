a3cf6554fde4f74c103d5916f47307916d94bd80
Sahil Amol Mutha <144716534+sahilmutha0711@users.noreply.github.com>
2023-10-23 15:59:17 -0400

    Committing src folder

---
+++ src/models/TeacherProfile.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author sahil
+ */
+public class TeacherProfile {
+    
+    private String Title;
+    private String FullName;
+    private String EmailId;
+    private String MobNo;
+    private String Username;
+    private String Password;
+    private String Age;
+    private String Gender;
+    
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String FullName) {
+        this.FullName = FullName;
+    }
+
+    public String getEmailId() {
+        return EmailId;
+    }
+
+    public void setEmailId(String EmailId) {
+        this.EmailId = EmailId;
+    }
+
+    public String getMobNo() {
+        return MobNo;
+    }
+
+    public void setMobNo(String MobNo) {
+        this.MobNo = MobNo;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+    
+}
